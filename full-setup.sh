30d4f558d6c0060a93cb2431801d8d0ab663a04f
raviteja <ravitejatfcdemo@gmail.com>
2023-02-04 06:29:30 +0530

    adding Jenkinsfile

--- full-setup.sh
+++ full-setup.sh
@@ -1,5 +1,5 @@
 for component in frontend cart catalogue user shipping payment dispatch redis mongodb mysql rabbitmq ; do
 
-  ansible-playbook -i ${component}.raviteja.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
+  ansible-playbook -i ${component}-dev.raviteja.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
 
 done
